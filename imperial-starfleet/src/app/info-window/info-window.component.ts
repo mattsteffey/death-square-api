a79ea91d7803b348f5cec4959a77e149d3dccd43
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2018-02-28 10:29:27 -0700

    finished 8

--- imperial-starfleet/src/app/info-window/info-window.component.ts
+++ imperial-starfleet/src/app/info-window/info-window.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-info-window',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoWindowComponent implements OnInit {
 
-  constructor() { }
+	dataBanks = {};
+
+  constructor(private http: Http) { }
+
+  findTurret(turret) {
+  	console.log(`finding ${turret}`);
+  	this.http.get('http://localhost:3000/api/turret/'+turret)
+  		.toPromise()
+  		.then( (res)=>  this.dataBanks = res.json() );
+  }
 
   ngOnInit() {
+  	this.findTurret(4)
   }
 
 }
